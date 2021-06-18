26335ce44dd96e593d6d1ccea55bd2372869df59
julia-as <julia.e.as@gmail.com>
2021-06-18 11:25:44 +0200

    added cors setting

--- src/main/java/de/julia/kitabase/controller/ChildController.java
+++ src/main/java/de/julia/kitabase/controller/ChildController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import de.julia.kitabase.exceptions.ChildNotFoundException;
 import de.julia.kitabase.model.Child;
 import de.julia.kitabase.repository.ChildRepository;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ChildController {
 	
